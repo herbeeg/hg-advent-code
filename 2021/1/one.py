fc18b3f969fba150b593ba562bd287c17675266f
Jon Herbst <herbtaculargames@gmail.com>
2022-01-21 12:41:31 +0000

    Completing second part of Day 1 Sonar Sweep problem with comments

--- 2021/1/one.py
+++ 2021/1/one.py
@@ -1,6 +1,13 @@
 import sys
 
+from functools import reduce
+
 class DepthScan:
+    """
+    Day 1: Sonar Sweep
+
+    Part 1
+    """
     def __init__(self, input_file=''):
         self.filename = input_file
 
@@ -26,4 +33,52 @@ class DepthScan:
 
         return increases
 
+class TripleDepthScan:
+    """
+    Day 1: Sonar Sweep
+
+    Part 2
+    """
+    def __init__(self, input_file=''):
+        self.filename = input_file
+
+    def measure(self):
+        increases = 0
+        previous = 0
+
+        buffer = [
+            -1,
+            -1,
+            -1
+        ]
+        """Pre-populating the buffer so that we don't have to verify the length at any point."""
+
+        if self.filename:
+            try:
+                with open(self.filename, 'r') as triple_depth_readings:
+                    for reading in triple_depth_readings:
+                        buffer.pop(0)
+                        buffer.append(int(reading))
+
+                        if -1 not in buffer:
+                            """Only begin once previously populated values have been replaced."""
+                            triple = reduce(lambda x, y: x+y, buffer)
+                            """Reduce triplets down to a sum of all integer list elements."""
+
+                            if 0 < previous:
+                                """Start once we have an existing triple."""
+                                if previous < triple:
+                                    increases += 1
+
+                            previous = triple
+            except OSError as ex:
+                """Terminate if an invalid path has been provided."""
+                sys.stdout.write(ex.strerror + '\n')
+                sys.exit()
+
+        return increases
+
 print(DepthScan('input.txt').measure())
+"""P1."""
+print(TripleDepthScan('input.txt').measure())
+"""P2."""
