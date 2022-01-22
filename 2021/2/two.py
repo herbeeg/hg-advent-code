5df8b4926c3f4398b25950f1c03130bc697916fa
Jon Herbst <herbtaculargames@gmail.com>
2022-01-22 16:55:10 +0000

    Compile day 2 part 2 solution with separate class so that solutions can be containerised and run individually instead of avoiding duplicate code

--- 2021/2/two.py
+++ 2021/2/two.py
@@ -55,5 +55,57 @@ class PlannedCourse:
             'up'
         ]
 
+class CourseAim:
+    """
+    Day 2: Dive!
+
+    Part 2
+    """
+    def __init__(self, input_file=''):
+        self.filename = input_file
+    
+    def multiply(self) -> int:
+        values = self.getCourseReadings()
+
+        return values[0] * values[1]
+
+    def getCourseReadings(self) -> tuple:
+        h_pos = 0
+        depth = 0
+
+        aim = 0
+
+        if self.filename:
+            try:
+                with open(self.filename, 'r') as navigation_instructions:
+                    for instruction in navigation_instructions:
+                        parts = instruction.rstrip().split(' ')
+                        """Remove any unwanted whitspace or trailing newline characters for clean parsing of navigation data."""
+
+                        if parts and parts[0] in self.validInstructions():
+                            if 'forward' == parts[0]:
+                                h_pos += int(parts[1])
+                                depth += int(parts[1]) * aim
+                            elif 'down' == parts[0]:
+                                aim += int(parts[1])
+                            elif 'up' == parts[0]:
+                                aim -= int(parts[1])
+
+            except OSError as ex:
+                """Terminate if an invalid path has been provided."""
+                sys.stdout.write(ex.strerror + '\n')
+                sys.exit()
+
+        return (h_pos, depth)
+
+    def validInstructions(self) -> list:
+        return [
+            'forward',
+            'down',
+            'up'
+        ]
+
 print(PlannedCourse('input.txt').multiply())
 """P1."""
+print(CourseAim('input.txt').multiply())
+"""P2."""
