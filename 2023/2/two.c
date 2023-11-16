db602aa624fd27d0cefb1180446d3ca12dc957de
Jon Herbst <herbtaculargames@gmail.com>
2023-11-16 11:08:06 +0000

    Modifying the existing score function to calculate a new total based on the second strategy guide and printing out those new totals

--- 2023/2/two.c
+++ 2023/2/two.c
@@ -8,38 +8,70 @@
  * strcmp requires a pointer to a sequence of characters that end
  * with a 0 so we can swap to using a direct char comparison
  * instead, with some so simple such as '=='.
+ *
+ * Also includes a new strategy guide based on the second column of the
+ * input denoting whether the player should win, lose
+ * or draw the match of rock, paper, scissors.
 */
-int Get_Score(char opponent, char player)
+int Get_Score(char opponent, char player, int ident)
 {
   int score = 0;
+  int secret_score = 0;
 
   if ('A' == opponent) {
-    if ('X' == player) {
-      score = 4;
-    } else if ('Y' == player) {
-      score = 8;
-    } else if ('Z' == player) {
-      score = 3;
+    switch (player) {
+      case 'X':
+        score = 4;
+        secret_score = 3;
+        break;
+      case 'Y':
+        score = 8;
+        secret_score = 4;
+        break;
+      case 'Z':
+        score = 3;
+        secret_score = 8;
+        break;
     }
   } else if ('B' == opponent) {
-    if ('X' == player) {
-      score = 1;
-    } else if ('Y' == player) {
-      score = 5;
-    } else if ('Z' == player) {
-      score = 9;
+    switch (player) {
+      case 'X':
+        score = 1;
+        secret_score = 1;
+        break;
+      case 'Y':
+        score = 5;
+        secret_score = 5;
+        break;
+      case 'Z':
+        score = 9;
+        secret_score = 9;
+        break;
     }
   } else if ('C' == opponent) {
-    if ('X' == player) {
-      score = 7;
-    } else if ('Y' == player) {
-      score = 2;
-    } else if ('Z' == player) {
-      score = 6;
+    switch (player) {
+      case 'X':
+        score = 7;
+        secret_score = 2;
+        break;
+      case 'Y':
+        score = 2;
+        secret_score = 6;
+        break;
+      case 'Z':
+        score = 6;
+        secret_score = 7;
+        break;
     }
   }
 
-  return score;
+  if (1 == ident) {
+    return score;
+  } else if (2 == ident) {
+    return secret_score;
+  }
+
+  return 0;
 }
 
 int main()
@@ -53,6 +85,7 @@ int main()
   char buffer[2500];
 
   int total = 0;
+  int secret_total = 0;
 
   while (NULL != fgets(buffer, 2500, f)) {
     char c;
@@ -68,10 +101,12 @@ int main()
       }
     }
 
-    total = total + Get_Score(c, p);
+    total = total + Get_Score(c, p, 1);
+    secret_total = secret_total + Get_Score(c, p, 2);
   }
 
   printf("The total rock, paper, scissors score based on the strategy guide is %d\n", total);
+  printf("The total rock, paper, scissors score based on the top secret strategy guide is %d\n", secret_total);
 
   return 0;
 }
--- 2023/2/two.h
+++ 2023/2/two.h
@@ -1,6 +1,6 @@
 #ifndef __TWO_DOT_H__
 #define __TWO_DOT_H__
 
-extern int Get_Score(char opponent, char player);
+extern int Get_Score(char opponent, char player, int ident);
 
 #endif
