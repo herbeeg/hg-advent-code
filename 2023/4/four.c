7858a18cca5e22f7075f48aecea0d140c3955eab
Jon Herbst <herbtaculargames@gmail.com>
2023-11-22 11:33:05 +0000

    Resolution of first part of problem 2022/4

---
+++ 2023/4/four.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Allowing for more 'traditional' true/false checks.
+typedef enum { false, true } bool;
+
+int main()
+{
+  FILE *f = fopen("input.txt", "r");
+
+  if (NULL == f) {
+    exit(0);
+  }
+
+  char buffer[1000];
+
+  char delim[2];
+  delim[0] = ',';
+  // Null-termination of string is important during the comparison.
+  delim[1] = '\0';
+
+  char elf_delim[2];
+  elf_delim[0] = '-';
+  elf_delim[1] = '\0';
+
+  int overlaps = 0;
+
+  while (NULL != fgets(buffer, 1000, f)) {
+    if ('\n' != buffer[0]) {
+      // Store a copy of the splitting of the string as the
+      // strtok function modifies the original value.
+      char *tmp = strtok(buffer, delim);
+      char *first_elf = tmp;
+
+      // Passing null to the function automatically moves to the
+      // following occurence using the delimiter as the
+      // pointer is preserved in memory.
+      tmp = strtok(NULL, delim);
+
+      char *second_elf = tmp;
+
+      // Allowing the conversion of character strings to integers.
+      int first_min_id = atoi(strtok(first_elf, elf_delim));
+      int first_max_id = atoi(strtok(NULL, elf_delim));
+
+      int second_min_id = atoi(strtok(second_elf, elf_delim));
+      int second_max_id = atoi(strtok(NULL, elf_delim));
+
+      // The custom type definition is a nice feature, allowing
+      // for customisable types and values to emulate
+      // features that might occur more often in
+      // other languages.
+      bool b = false;
+
+      if (second_min_id <= first_min_id) {
+        if (second_max_id >= first_max_id) {
+          overlaps++;
+          b = true;
+        }
+      }
+
+      // If the second part of the first if statement doesn't
+      // pass, then we still want to run the reverse.
+      if (true != b) {
+        if (first_min_id <= second_min_id) {
+          if (first_max_id >= second_max_id) {
+            overlaps++;
+          }
+        }
+      }
+    }
+  }
+
+  printf("The number of assignment pairs where one fully contains the other is %d\n", overlaps);
+
+  return 0;
+}
