beb8063ed442e1eabc25dd2cc1a74af03e9c0bb2
Jon Herbst <herbtaculargames@gmail.com>
2023-12-14 22:16:50 +0000

    Changing the input to live and forcing double checks on the register value and cycles so that we do not have the opportunity to skip over one of the required 20/60/100/140/180/220 position checks

--- 2022/10/ten.c
+++ 2022/10/ten.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+int passed_cycles[6];
+int passed_ptr = 0;
+
+long int Check_Cycles(int count, long int strength, long int reg)
+{
+  for (int p = 0; p < 6; p++) {
+    if (count == passed_cycles[p]) {
+      return strength;
+    }
+  }
+  
+  passed_cycles[passed_ptr] = count;
+  passed_ptr++;
+  
+  printf("%li\n", strength);
+  printf("%li\n", reg);
+  
+  return strength + (count * reg);
+}
+
 int main()
 {
   FILE *f = fopen("input.txt", "r");
@@ -12,30 +32,69 @@ int main()
   
   char buffer[150];
   
-  int register_x = 1;
-  int cycle = 0;
-  
-  // The commands to be executed on the next cycle.
-  int queue_next[50];
-  // The queued commands to be deferred by two cycles.
-  int queue_defer[50];
+  // Using the long int data type here to represent the register value
+  // to defend against any moments that could arise from the total
+  // exceeding or going below [-32,767, +32,767].
+  long int register_x = 1;
+  long int current_cycle = 1;
+  long int total_signal_strength = 0;
   
   char cycle_delim[2];
   cycle_delim[0] = ' ';
-  cycle_delim[1] = '\n';
+  cycle_delim[1] = '\0';
   
   while (NULL != fgets(buffer, 150, f)) {
     if ('\n' != buffer[0]) {
       char *tmp_buffer = buffer;
+      long int register_change = 0;
       
-      if (0 == strncmp("noop", tmp_buffer, 4)) {
-        // Execute any commands in the queue and skip to the next cycle.
-      } else {
+      if (20 == current_cycle) {
+        total_signal_strength = Check_Cycles(20, total_signal_strength, register_x);
+      } else if (60 == current_cycle) {
+        total_signal_strength = Check_Cycles(60, total_signal_strength, register_x);
+      } else if (100 == current_cycle) {
+        total_signal_strength = Check_Cycles(100, total_signal_strength, register_x);
+      } else if (140 == current_cycle) {
+        total_signal_strength = Check_Cycles(140, total_signal_strength, register_x);
+      } else if (180 == current_cycle) {
+        total_signal_strength = Check_Cycles(180, total_signal_strength, register_x);
+      } else if (220 == current_cycle) {
+        total_signal_strength = Check_Cycles(220, total_signal_strength, register_x);
+      }
+      
+      if (0 == strncmp("addx", tmp_buffer, 4)) {
         // Prepare to add the next register command to the deferred queue and
         // continue to the next cycle as before.
+        tmp_buffer[strlen(tmp_buffer) - 1] = '\0';
+        
+        char *add_val = strtok(tmp_buffer, cycle_delim);
+        add_val = strtok(NULL, cycle_delim);
+        
+        register_change = atol(add_val);
+        current_cycle++;
       }
+      
+      if (20 == current_cycle) {
+        total_signal_strength = Check_Cycles(20, total_signal_strength, register_x);
+      } else if (60 == current_cycle) {
+        total_signal_strength = Check_Cycles(60, total_signal_strength, register_x);
+      } else if (100 == current_cycle) {
+        total_signal_strength = Check_Cycles(100, total_signal_strength, register_x);
+      } else if (140 == current_cycle) {
+        total_signal_strength = Check_Cycles(140, total_signal_strength, register_x);
+      } else if (180 == current_cycle) {
+        total_signal_strength = Check_Cycles(180, total_signal_strength, register_x);
+      } else if (220 == current_cycle) {
+        total_signal_strength = Check_Cycles(220, total_signal_strength, register_x);
+      }
+      
+      register_x += register_change;
+      
+      current_cycle++;
     }
   }
+  
+  printf("The sum of the six signal strengths is %li", total_signal_strength);
   
   return 0;
 }
---
+++ 2022/10/ten.h
@@ -0,0 +1,6 @@
+#ifndef __TEN_DOT_H__
+#define __TEN_DOT_H__
+
+extern long int Check_Cycles(int count, long int strength, long int reg);
+
+#endif
