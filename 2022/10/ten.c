8220a8c9e778b3046853e891701e2d20c138d6c5
Jon Herbst <herbtaculargames@gmail.com>
2023-12-19 16:06:21 +0000

    Replacing input with test data and almost coming up with a solution for problem 10 part 2 but currently having timing issues when writing certain values to the output CRT buffer which require modifications similar to the cycle check in P1

--- 2022/10/ten.c
+++ 2022/10/ten.c
@@ -5,6 +5,58 @@
 int passed_cycles[6];
 int passed_ptr = 0;
 
+char crt[6][41];
+
+void Fill_CRT_Screen_With_Defaults(char matrix[6][41])
+{
+  char autofill[41];
+  autofill[0] = '.';
+  autofill[1] = '.';
+  autofill[2] = '.';
+  autofill[3] = '.';
+  autofill[4] = '.';
+  autofill[5] = '.';
+  autofill[6] = '.';
+  autofill[7] = '.';
+  autofill[8] = '.';
+  autofill[9] = '.';
+  autofill[10] = '.';
+  autofill[11] = '.';
+  autofill[12] = '.';
+  autofill[13] = '.';
+  autofill[14] = '.';
+  autofill[15] = '.';
+  autofill[16] = '.';
+  autofill[17] = '.';
+  autofill[18] = '.';
+  autofill[19] = '.';
+  autofill[20] = '.';
+  autofill[21] = '.';
+  autofill[22] = '.';
+  autofill[23] = '.';
+  autofill[24] = '.';
+  autofill[25] = '.';
+  autofill[26] = '.';
+  autofill[27] = '.';
+  autofill[28] = '.';
+  autofill[29] = '.';
+  autofill[30] = '.';
+  autofill[31] = '.';
+  autofill[32] = '.';
+  autofill[33] = '.';
+  autofill[34] = '.';
+  autofill[35] = '.';
+  autofill[36] = '.';
+  autofill[37] = '.';
+  autofill[38] = '.';
+  autofill[39] = '.';
+  autofill[40] = '\n';
+
+  for (int c = 0; 6 > c; c++) {
+    strcpy(matrix[c], autofill);
+  }
+}
+
 long int Check_Cycles(int count, long int strength, long int reg)
 {
   for (int p = 0; p < 6; p++) {
@@ -16,10 +68,24 @@ long int Check_Cycles(int count, long int strength, long int reg)
   passed_cycles[passed_ptr] = count;
   passed_ptr++;
   
-  printf("%li\n", strength);
-  printf("%li\n", reg);
-  
   return strength + (count * reg);
+}
+
+void Draw_To_File(char matrix[6][41])
+{
+  FILE *f_out = fopen("output.txt", "w");
+  
+  if (NULL == f_out) {
+    exit(0);
+  }
+  
+  for (int m_row = 0; 6 > m_row; m_row++) {
+    for (int m_col = 0; 41 > m_col; m_col++) {
+      fprintf(f_out, "%c", matrix[m_row][m_col]);
+    }
+  }
+  
+  fclose(f_out);
 }
 
 int main()
@@ -38,6 +104,10 @@ int main()
   long int register_x = 1;
   long int current_cycle = 1;
   long int total_signal_strength = 0;
+  
+  // Fill the CRT buffer with dots so that when we need to draw a sprite
+  // to the simulated screen, we can just change it to a hash.
+  Fill_CRT_Screen_With_Defaults(crt);
   
   char cycle_delim[2];
   cycle_delim[0] = ' ';
@@ -62,6 +132,56 @@ int main()
         total_signal_strength = Check_Cycles(220, total_signal_strength, register_x);
       }
       
+      if (1 <= current_cycle && 40 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[0][new_cycle - 1] = '#';
+        }
+      } else if (41 <= current_cycle && 80 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[1][new_cycle - 1] = '#';
+        }
+      } else if (81 <= current_cycle && 120 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[2][new_cycle - 1] = '#';
+        }
+      } else if (121 <= current_cycle && 160 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[3][new_cycle - 1] = '#';
+        }
+      } else if (161 <= current_cycle && 200 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[4][new_cycle - 1] = '#';
+        }
+      } else if (201 <= current_cycle && 240 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[5][new_cycle - 1] = '#';
+        }
+      }
+      
       if (0 == strncmp("addx", tmp_buffer, 4)) {
         // Prepare to add the next register command to the deferred queue and
         // continue to the next cycle as before.
@@ -71,6 +191,7 @@ int main()
         add_val = strtok(NULL, cycle_delim);
         
         register_change = atol(add_val);
+        
         current_cycle++;
       }
       
@@ -90,10 +211,61 @@ int main()
       
       register_x += register_change;
       
+      if (1 <= current_cycle && 40 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[0][new_cycle - 1] = '#';
+        }
+      } else if (41 <= current_cycle && 80 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[1][new_cycle - 1] = '#';
+        }
+      } else if (81 <= current_cycle && 120 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[2][new_cycle - 1] = '#';
+        }
+      } else if (121 <= current_cycle && 160 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[3][new_cycle - 1] = '#';
+        }
+      } else if (161 <= current_cycle && 200 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[4][new_cycle - 1] = '#';
+        }
+      } else if (201 <= current_cycle && 240 >= current_cycle) {
+        int new_cycle = current_cycle % 40;
+        if (0 == new_cycle) {
+          new_cycle = 40;
+        }
+        if ((register_x - 1) <= new_cycle && (register_x + 1) >= new_cycle) {
+          crt[5][new_cycle - 1] = '#';
+        }
+      }
+      
       current_cycle++;
     }
   }
   
+  Draw_To_File(crt);
   printf("The sum of the six signal strengths is %li", total_signal_strength);
   
   return 0;
--- 2022/10/ten.h
+++ 2022/10/ten.h
@@ -1,6 +1,8 @@
 #ifndef __TEN_DOT_H__
 #define __TEN_DOT_H__
 
+extern void Fill_CRT_Screen_With_Defaults(char matrix[6][41]);
 extern long int Check_Cycles(int count, long int strength, long int reg);
+extern void Draw_To_File(char matrix[6][41]);
 
 #endif
